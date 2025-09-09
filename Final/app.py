82cf57d6a83e3c4b8404425ee4ed07deee7cbfb3
Pranav Prathap <prnvprthp@gmail.com>
2025-09-09 15:29:06 -0400

    Update v4

--- Final/app.py
+++ Final/app.py
@@ -1,16 +1,16 @@
 from dash import Dash, html, page_container
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__,use_pages=True,    suppress_callback_exceptions=True,title="Final Project")
+app = Dash(__name__,use_pages=True, external_stylesheets=[dbc.themes.FLATLY],suppress_callback_exceptions=True,title="Final Project")
 server = app.server
 
 app.layout = html.Div([
     dbc.NavbarSimple(
         children=[
             dbc.NavLink("Home", href="/", active="exact"),
-            dbc.NavLink("Page One", href="/page1", active="exact"),
-            dbc.NavLink("Page 2", href="/page2", active="exact"),
-            dbc.NavLink("Page 3", href="/page3", active="exact")
+            dbc.NavLink("Page One", href="/FinalPage1", active="exact"),
+            dbc.NavLink("Page 2", href="/FinalPage2", active="exact"),
+            dbc.NavLink("Page 3", href="/FinalPage3", active="exact")
         ],
         brand="Employment Dashboard",
         color="primary",
--- Final/pages/home.py
+++
@@ -1,8 +0,0 @@
-import dash
-from dash import html
-
-dash.register_page(__name__, path="/", name="Home")
-
-layout = html.Div([
-    html.H1("Analyzing Economic and Employment Trends by Industry and US states")
-    ], className='page1-introduction')
